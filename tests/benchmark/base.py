37713b8ebab31d3189af44713e1bb89ca0d24284
Baohua Yang <yangbaohua@gmail.com>
2016-06-18 16:15:58 +0800

    Add base benchmark

---
+++ tests/benchmark/base.py
@@ -0,0 +1,15 @@
+
+import requests
+
+import timeit
+
+
+def test(url="http://127.0.0.1:80"):
+    r = requests.get(url)
+
+if __name__ == '__main__':
+    print("Test starting...try 2000 times")
+    duration=timeit.timeit("test()", number=2000,
+                           setup="from __main__ import test")
+    print("2000 times done, the time={}".format(duration))
+
